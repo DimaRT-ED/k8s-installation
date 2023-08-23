35faba15aedead3e0cd4f1ee67296d23ed55488b
Ilay Rubin <108349408+ilay-rubin@users.noreply.github.com>
2023-08-23 12:31:48 +0300

    Update K8S-WorkerSetup2.sh

--- K8S-WorkerSetup2.sh
+++ K8S-WorkerSetup2.sh
@@ -41,3 +41,7 @@ sudo apt update;
 sudo apt install -y kubelet=1.26.0-00 kubeadm=1.26.0-00 kubectl=1.26.0-00;
 
 sudo apt-mark hold kubelet kubeadm kubectl;
+
+echo -n "Return to the "Master Node" machine.
+
+echo -e "1. Return to the \033[1mMaster Node\033[0m machine.\n\n2. Copy the contents of the \033[1mkubeadm-join\033[0m command that we received from the Master Node. If you have lost this content, you can generate new content by running the following command on the Master Node:\n\n\033[1mkubeadm token create --print-join-command\033[0m\n\n3. After obtaining the join command, prepend it with \033[1msudo\033[0m and append the flag \033[1m--cri-socket=unix:///var/run/cri-dockerd.sock\033[0m.\n\nFor example, the modified join command should look like:\n\n\033[1msudo kubeadm join 192.168.0.10:6443 --token nzy73q.3s2ig8r2ue09wrjy --discovery-token-ca-cert-hash sha256:0866420e1833c5a0710c92712d56468b4576b8bd8f00c381232dd993d89a0246 --cri-socket=unix:///var/run/cri-dockerd.sock\033[0m\n\n4. Copy the modified command, then paste and execute it in the terminal of each of the \033[1mWorker Nodes\033[0m (the two workers we created).\n\n5. After joining all the worker nodes, switch back to the \033[1mMaster Node\033[0m terminal and verify the nodes by running:\n\n\033[1mkubectl get nodes\033[0m"
